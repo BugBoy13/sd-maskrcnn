1cc29a0313f73d5326284cc0bd6c6f97d99e3589
vardaan <vardaan13121997@gmail.com>
2020-04-13 17:43:50 +0530

    Tensorflow change

--- tools/train.py
+++ tools/train.py
@@ -1,26 +1,3 @@
-"""
-Copyright ©2019. The Regents of the University of California (Regents). All Rights Reserved.
-Permission to use, copy, modify, and distribute this software and its documentation for educational,
-research, and not-for-profit purposes, without fee and without a signed licensing agreement, is
-hereby granted, provided that the above copyright notice, this paragraph and the following two
-paragraphs appear in all copies, modifications, and distributions. Contact The Office of Technology
-Licensing, UC Berkeley, 2150 Shattuck Avenue, Suite 510, Berkeley, CA 94720-1620, (510) 643-
-7201, otl@berkeley.edu, http://ipira.berkeley.edu/industry-info for commercial licensing opportunities.
-
-IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
-INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
-THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS BEEN
-ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
-HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE
-MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-
-Author: Mike Danielczuk
-"""
-
 import os
 import sys
 import time
@@ -124,8 +101,8 @@ if __name__ == "__main__":
     os.environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1'
 
     # Set up tf session to use what GPU mem it needs and train
-    tf_config = tf.ConfigProto()
+    tf_config = tf.compat.v1.ConfigProto()
     tf_config.gpu_options.allow_growth = True
-    with tf.Session(config=tf_config) as sess:
-        set_session(sess)
+    with tf.compat.v1.Session(config=tf_config) as sess:
+        tf.compat.v1.keras.backend.set_session(sess)
         train(config)
